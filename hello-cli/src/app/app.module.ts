9b7dc88b50e61216ca11859fd2f928cedb9b6dc0
Sean Coleman <coleman07@gmail.com>
2020-01-29 01:37:15 -0700

    product service -- hello cli

--- hello-cli/src/app/app.module.ts
+++ hello-cli/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ProductService } from './shared/product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
